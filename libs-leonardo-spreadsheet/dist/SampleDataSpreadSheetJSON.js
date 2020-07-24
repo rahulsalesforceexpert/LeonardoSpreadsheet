d4123c08fe16838f1f31a9b73114e3db9d41711d
rahulsalesforceexpert <55208367+rahulsalesforceexpert@users.noreply.github.com>
2020-07-24 14:20:44 +0530

    Sample JSON data script

---
+++ libs-leonardo-spreadsheet/dist/SampleDataSpreadSheetJSON.js
@@ -0,0 +1,168 @@
+let dataJSON = {
+  "@schemaVersion": "0.1",
+  "activeSheet": "Sheet1",
+  "names": [
+    {
+      "range": "Sheet1!$B$3:$C$4",
+      "name": "range1"
+    },
+    {
+      "range": "Sheet1!$C$7:$D$8",
+      "name": "range2"
+    }
+  ],
+  "defaults": {
+    "cellStyle": {
+      "background": "#ddebf7",
+      "wrap": true,
+      "textAlign": "center",
+      "verticalAlign": "top"
+    },
+    "cellFontAttrs": {
+      "family": "Calibri",
+      "size": "11",
+      "color": "#000000",
+      "bold": false,
+      "italic": false
+    }
+  },
+  "#sheets": [
+    {
+      "name": "Sheet1",
+      "id": "sidZ29puaK",
+      "maxColIndexEdited": 4,
+      "maxRowIndexEdited": 6,
+      "selection": "A1:B1",
+      "activeCell": "A1",
+      "frozenRows": 0,
+      "frozenColumns": 0,
+      "showGridLines": true,
+      "colHeader": false,
+      "rowHeader": false,
+      "defaults": {
+        "columnWidth": 64,
+        "rowHeight": 20,
+        "cellStyle": {
+          "background": "#ddebf7",
+          "wrap": true,
+          "textAlign": "center",
+          "verticalAlign": "top"
+        },
+        "cellFontAttrs": {
+          "family": "Calibri",
+          "size": "11",
+          "color": "#000000",
+          "bold": false,
+          "italic": false
+        }
+      },
+      "#rows": [
+        {
+          "index": 0,
+          "visible": true,
+          "height": 25,
+          "#cells": [
+            {
+              "ref": "A1",
+              "value": "HelloWorld ",
+              "formula": "A2",
+              "index": 0,
+              "fontAttrs": {
+                "runs": [
+                  {
+                    "start": 0,
+                    "txt": "He",
+                    "color": "#000000",
+                    "italic": false,
+                    "bold": false
+                  },
+                  {
+                    "start": 2,
+                    "txt": "llo ",
+                    "color": "#000000",
+                    "italic": true,
+                    "bold": false
+                  },
+                  {
+                    "start": 6,
+                    "txt": "W",
+                    "color": "#FF0000",
+                    "italic": true,
+                    "bold": true
+                  },
+                  {
+                    "start": 7,
+                    "txt": "orld ",
+                    "color": "#FF0000",
+                    "italic": false,
+                    "bold": true
+                  },
+                  {
+                    "start": 12,
+                    "txt": "!!!",
+                    "color": "#000000",
+                    "italic": false,
+                    "bold": false
+                  }
+                ]
+              },
+              "style": {
+                "border": {
+                  "left": {
+                    "clr": "#1f4e78",
+                    "type": "medium"
+                  },
+                  "right": {
+                    "clr": "#1f4e78",
+                    "type": "medium"
+                  },
+                  "bottom": {
+                    "clr": "#1f4e78",
+                    "type": "medium"
+                  },
+                  "top": {
+                    "clr": "#1f4e78",
+                    "type": "medium"
+                  }
+                }
+              }
+            }
+          ]
+        },
+        {
+          "index": 1,
+          "visible": true,
+          "height": 25,
+          "#cells": [
+            {
+              "ref": "A2",
+              "index": 0
+            }
+          ]
+        }
+      ],
+      "#columns": [
+        {
+          "visible": true,
+          "width": 60,
+          "index": 0
+        },
+        {
+          "visible": true,
+          "width": 60,
+          "index": 1
+        }
+      ],
+      "names": [
+        {
+          "range": "Sheet1!$A$1:$A$3",
+          "name": "range3"
+        },
+        {
+          "range": "Sheet1!$B$5:$B$8",
+          "name": "range4"
+        }
+      ]
+    }
+  ]
+}
